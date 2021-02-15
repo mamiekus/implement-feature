fefe9d708dba40283dfc877ca9bc2cb0181cff50
MM <maciek.miekus@northgravity.com>
2021-02-15 10:59:14 +0100

    fix

--- src/main/java/eu/europa/ec/digit/search/Application.java
+++ src/main/java/eu/europa/ec/digit/search/Application.java
@@ -1,54 +1,46 @@
 package eu.europa.ec.digit.search;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
 
-		LOGGER.info("Starting");
-		System.exit(0);
-	}
+        LOGGER.info("Starting");
+        System.exit(0);
+    }
 
-	protected String sortMyString(String unsortedString) {
+    protected String sortMyString(String unsortedString) {
 
-		List<String> list = Arrays.asList(unsortedString.split(" "));
-		Collections.sort(list);
-		return list.get(0) + " " + list.get(1) + " " + list.get(2) + " " + list.get(4) + " " + list.get(5) + " " + list.get(6) + " " + list.get(7) + " " + list.get(8);
+        List<String> list = Arrays.asList(unsortedString.split(" "));
+        Collections.sort(list, Comparator.comparing(String::toLowerCase));
+        return String.join(" ", list);
 
-	}
+    }
 
-	protected List<String> removeDuplicatesFromList(List<String> values) {
+    protected List<String> removeDuplicatesFromList(List<String> values) {
 
-		boolean exist = false;
-
-		for (String val : values) {
-
-			if (exist) {
-				values.remove(val);
-			} else {
-				exist = true;
-			}
-
-		}
-
-		return values;
-	}
+        return values.stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
 
 }
